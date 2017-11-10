d1750cd6768a3757101b438c9f3fec24349a638e
BalckMix <mixchatmix3@gmail.com>
2017-11-10 03:48:16 -0300

    tests coming

---
+++ tests/Login/LoginTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Login;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+
+class ExampleTest extends TestCase
+{
+    /*
+     * A basic test example.
+     *
+     * @return void
+     
+    public function testBasicExample()
+    {
+        $response = $this->withHeaders([
+            'Authorization' => 'Value',
+        ])->json('POST', '/api/v1/auth/login', ['email' => 'test@test.com', 'password' => '123456']);
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'created' => 'true',
+            ]);
+    }
+    */
+}
